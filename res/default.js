e788ee67564c59504bfbea8964c62f7a6bcdbf13
Roman Gräf <roman.graef@gmail.com>
2017-11-01 12:39:11 +0100

    Add files via upload

---
+++ res/default.js
@@ -0,0 +1,25 @@
+function sidebar_close() {
+    $("#sidebar").fadeOut();
+}
+
+function sidebar_open() {
+    $("#sidebar").fadeIn();
+}
+
+function show_loading() {
+	$("#content").html(("<div class='spinner centered'></div>"));
+}
+
+function load(file, title) {
+	show_loading();
+	$("#header").text(title);
+	$.get("file:///H:/ichtml/content/"+file, function(data) {
+		$("#content").html(data);
+	}, "text");
+}
+
+function open_timetable() {
+	sidebar_close();
+	load('timetable.html', 'Stundenplan');
+}
+
